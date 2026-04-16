3cd1aaa646aa0295c882fcfccdb32c15beb5b44d
Amro145 <amroaltayeb14@gmail.com>
2026-04-17 01:14:24 +0200

    chore: update Kafka client configuration to include additional broker addresses across all services

--- kafka/admin.js
+++ kafka/admin.js
@@ -2,28 +2,29 @@ import { Kafka } from "kafkajs";
 
 const kafka = new Kafka({
     clientId: "kafka-service",
-    brokers: ["localhost:9094"],
+    brokers: ["localhost:9094", "localhost:9095", "localhost:9096"],
+
 })
 
 const admin = kafka.admin();
 
 async function run() {
     await admin.connect();
-    
+
     const existingTopics = await admin.listTopics();
     const requiredTopics = ["payment-successful", "order-successful", "email-successful"];
-    
+
     const topicsToCreate = requiredTopics
         .filter(topic => !existingTopics.includes(topic))
         .map(topic => ({ topic }));
-        
+
     if (topicsToCreate.length > 0) {
         await admin.createTopics({ topics: topicsToCreate });
         console.log("Topics created:", topicsToCreate.map(t => t.topic));
     } else {
         console.log("All topics already exist.");
     }
-    
+
     await admin.disconnect();
 }
 
